583713fb967054e791af16701f8fa1fa9659ccea
Grant Michael Gutterman <grant.gutterman@stu.jefferson.kyschools.us>
2018-09-17 15:05:23 -0400

    boi if you dont quit wearing those fake yeezys....

---
+++ dictionary.py
@@ -0,0 +1,12 @@
+#Importations
+
+import random
+import time
+import sys
+
+gamertag = input("Welcome to Halo Chronicles! Type in your gamertag or name: ")
+
+weapons = {
+    infantry_weapons = ["Assault Rifle", "Battle Rifle", "Plasma Rifle", "Submachine Gun", "Saw", "Rocket Launcher"]
+    vehicle_weapons = ["Machine Gun", "Rocket Shots", "Gauss Gun"]
+}
